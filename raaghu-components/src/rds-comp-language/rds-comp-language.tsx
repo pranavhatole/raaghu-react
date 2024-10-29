7622f5cd645e070e9b81c79ab2903e3692d84d30
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-10-29 12:19:41 +0530

    Commit Changes for bug 972

--- raaghu-components/src/rds-comp-language/rds-comp-language.tsx
+++ raaghu-components/src/rds-comp-language/rds-comp-language.tsx
@@ -168,7 +168,7 @@ const RdsCompLanguage = (props: RdsCompLanguageProps) => {
                         </div>
                     </div>
                 </div>
-                <div className="d-flex flex-column-reverse ps-4 flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3 pb-3">
+                <div className="d-flex flex-column-reverse ps-4 flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3 pb-3 p-4">
                     <RdsButton
                         label="Cancel"
                         type="button"
